1f2e14a40e4710d8d201271c7887f0306203491d
shniubobo <shniubobo@outlook.com>
2024-12-21 04:41:51 +0000

    ✨(ui) Create draft implementation
    Changelog: skip

---
+++ pgpvis-ui/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+import { parse_armored } from "pgpvis-core";
+
+const message = ref("");
+const packets = ref<Map<string, string>>();
+</script>
+
+<template>
+  <textarea v-model="message"></textarea>
+  <button @click="packets = parse_armored(message)">Parse</button>
+  <li v-for="[index, packet] in packets" :key="index">{{ index }} | {{ packet }}</li>
+</template>
+
+<style scoped></style>
---
+++ pgpvis-ui/vite.config.ts
@@ -0,0 +1,17 @@
+import { fileURLToPath, URL } from "node:url";
+
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
+import topLevelAwait from "vite-plugin-top-level-await";
+import vueDevTools from "vite-plugin-vue-devtools";
+import wasm from "vite-plugin-wasm";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueDevTools(), wasm(), topLevelAwait()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
