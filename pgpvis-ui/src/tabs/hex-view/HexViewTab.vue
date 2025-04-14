33adf114a2caac4a5d71ce9e92cf5542edd858fa
shniubobo <me@shniubobo.com>
2025-04-14 16:41:50 +0100

    ✨(ui) Virtualize the hex view
    The hex view, with the last commit, contains a large number of
    `<span>`s, which makes the web page laggy. This commit addresses that
    issue by virtualizing the hex view.
    
    `@tanstack/vue-virtual` has been used instead of `vue-virtual-scroller`,
    because the former is more actively maintained (the latter has had no
    release since more than two years ago, and that last release was a beta
    one), and the latter has no typescript support.

--- pgpvis-ui/src/tabs/hex-view/HexViewTab.vue
+++ pgpvis-ui/src/tabs/hex-view/HexViewTab.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { computed, ref } from "vue";
+import { useVirtualizer } from "@tanstack/vue-virtual";
+import { computed, ref, useTemplateRef } from "vue";
 
 import type { Line } from "./BytesLine.vue";
 import BytesLine from "./BytesLine.vue";
@@ -33,15 +34,66 @@ const byte_lines = computed(() => {
 function is_byte_selected(offset: number): boolean {
   return selected_byte_offsets.value.includes(offset);
 }
+
+const hex_view = useTemplateRef("hex-view");
+// `useVirtualizer` is not responsive over `count`, so we have to use
+// `computed` here.
+//
+// See:
+// - https://github.com/TanStack/virtual/issues/363
+// - https://github.com/TanStack/virtual/issues/969
+const virtual_hex_view = computed(() =>
+  useVirtualizer({
+    count: byte_lines.value.length,
+    getScrollElement: () => hex_view.value,
+    estimateSize: () => 24, // TODO: Do not hard-code this.
+  }),
+);
+const virtual_lines = computed(() =>
+  virtual_hex_view.value.value.getVirtualItems(),
+);
+
+// The virtualizer has a bug that, when it is updated with new `byte_lines`,
+// the old data is still on the screen, if the new data is shorter than the
+// viewport.
+//
+// To reproduce:
+//
+// 1. Paste a long message into the input tab and press "Parse"
+// 2. Scroll the hex view very fast, by dragging the scrollbar (the bug is not
+//    reproducible with slow scrolls with the mouse wheel)
+// 3. Paste a short message into the input tab and press "Parse" (to reproduce
+//    the bug, the resulting bytes in the hex view must not take up the whole
+//    tab)
+// 4. Some of the old bytes that are not covered by the new bytes are still on
+//    the screen. They are not selectable. Switching browser tabs, switching
+//    system windows, or minimizing and maximizing the browser window makes
+//    the old bytes disappear.
+//
+// TODO: Come up with a minimal reproducible example, file a bug report and
+// leave a link here to that issue.
 </script>
 
 <template>
-  <div class="gutter-stable h-screen overflow-auto p-4">
-    <BytesLine
-      v-for="(line, idx) in byte_lines"
-      :key="idx * LINE_LENGTH"
-      :line="line"
-      :offset="idx * LINE_LENGTH"
-    />
+  <div ref="hex-view" class="gutter-stable h-screen overflow-auto p-4">
+    <div
+      class="relative w-full"
+      :style="{ height: `${virtual_hex_view.value.getTotalSize()}px` }"
+    >
+      <div
+        v-for="virtual_line in virtual_lines"
+        :key="virtual_line.index"
+        class="absolute top-0 left-0 w-full"
+        :style="{
+          height: `${virtual_line.size}px`,
+          transform: `translateY(${virtual_line.start}px)`,
+        }"
+      >
+        <BytesLine
+          :line="byte_lines[virtual_line.index]"
+          :offset="virtual_line.index * LINE_LENGTH"
+        />
+      </div>
+    </div>
   </div>
 </template>
